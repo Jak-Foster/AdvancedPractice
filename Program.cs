cfe0540ee49585c2af3e59e65172830d49254006
JakFoster <jakfoster97@outlook.com>
2025-07-26 17:35:34 +0100

    Finished Asynchronous Random Words and Many Random Words challenges

--- Program.cs
+++ Program.cs
@@ -1,218 +1,55 @@
-﻿using AdvancedPractice.McDroid;
-using AdvancedPractice.IField;
-using IFieldPig = AdvancedPractice.IField.Pig;
-using McDroidPig = AdvancedPractice.McDroid.Pig;
-using AdvancedPractice.The_Sieve;
-using AdvancedPractice.CharberryTree;
-using System.Security.Cryptography.X509Certificates;
-using AdvancedPractice.PatternPotions;
-using AdvancedPractice.OperandCity;
-using AdvancedPractice.TheThreeLenses;
-using AdvancedPractice.The_Repeating_Stream;
+﻿
+
+
 internal class Program
 {
-    private static void Main(string[] args)
+    private static async Task Main(string[] args)
     {
-        //Cow Cow = new();
-        //Sheep Sheep = new();
-        //IFieldPig FieldPig = new();
-        //McDroidPig DroidPig = new();
-
-        //Console.WriteLine("Pick one of three filters to check whether a number is good or bad:");
-        //Console.WriteLine("IsEven, IsPositive, or IsMultipleOfTen");
-
-        //string? UserInput = Console.ReadLine();
-        //Sieve Sieve;
-
-        //if (UserInput == "IsEven") Sieve = new(X => X % 2 == 0);
-        //else if (UserInput == "IsMultipleOfTen") Sieve = new(X => X % 10 == 0);
-        //else if (UserInput == "IsPositive") Sieve = new(X => X >= 0);
-        //else
-        //{
-        //    Console.WriteLine("Invalid input. Run the program again.");
-        //    return;
-        //}
-
-        //while (true)
-        //{
-        //    Console.WriteLine("--------------------\n\r");
-
-        //    string? UserNumberInput = Console.ReadLine();
-
-        //    try
-        //    {
-        //        int ConvertedInput = Convert.ToInt32(UserNumberInput);
-
-        //        bool Response = Sieve.IsGood(ConvertedInput);
-
-        //        if (Response)
-        //        {
-        //            Console.WriteLine("Good number entered!");
-        //        }
-        //        else Console.WriteLine("Bad number entered :(");
-        //    }
-        //    catch
-        //    {
-        //        Console.WriteLine("Invalid number entered. Try again.");
-        //    }
-        //}
-
-        //CharberryTree Tree = new();
-        //Notifier Notifier = new(Tree);
-        //Harvester Harvester = new(Tree);
-
-        //while(true)
-        //{
-        //    Tree.MaybeGrow();
-        //    //if(Tree.Ripe)
-        //    //    Console.WriteLine("Tree is ripe!");
-        //}
-
-        //Console.WriteLine("Please enter your name");
-        //string? UserName = Console.ReadLine();
-        //int UserScore = 0;
-
-        //if (File.Exists($"../../../UserScores/{UserName}.txt")) UserScore = Convert.ToInt32(File.ReadAllText($"../../../UserScores/{UserName}.txt"));
-
-        //Console.WriteLine("You get one point for every key press you make. Press enter to end the game. Begin!");
-        //while (Console.ReadKey().Key != ConsoleKey.Enter)
-        //{
-        //    UserScore++;
-        //    Console.WriteLine("Score: " + UserScore);
-        //}
-
-        //File.WriteAllText($"../../../UserScores/{UserName}.txt", UserScore.ToString());
-
-        //bool ContinueConcoctingPotion = true;
-        //Potion UserPotion = Potion.Water;
-
-        //while (ContinueConcoctingPotion)
-        //{
-        //    Console.WriteLine($"The potion you currently have is {UserPotion}");
-        //    Console.WriteLine("The available ingredients are:");
-        //    Console.WriteLine("Stardust, SnakeVenom, DragonBreath, ShadowGlass, EyeshineGem");
-        //    Console.WriteLine("Choose an ingredient...");
-
-        //    string? UserIngredientChoice = Console.ReadLine();
-
-        //    IngredientType Ingredient = UserIngredientChoice switch
-        //    {
-        //        "Stardust"     => IngredientType.Stardust,
-        //        "SnakeVenom"   => IngredientType.SnakeVenom,
-        //        "DragonBreath" => IngredientType.DragonBreath,
-        //        "ShadowGlass"  => IngredientType.ShadowGlass,
-        //        "EyeshineGem"  => IngredientType.EyeshineGem,
-        //        _              => IngredientType.AnythingElse
-        //    };
-
-        //    UserPotion = PotionCreator.DeterminePotion(UserPotion, Ingredient);
-
-        //    Console.WriteLine($"The potion you have concocted is a {UserPotion}!");
-        //    if (UserPotion == Potion.RuinedPotion)
-        //    {
-        //        Console.WriteLine("Starting again from scratch...");
-        //        UserPotion = Potion.Water;
-        //    }
-
-        //    Console.WriteLine("Do you want to continue concocting potions? yes/no");
-        //    if (Console.ReadLine() == "no") return;
-        //}
-
-        //BlockCoordinate BlockCoordinate = new(0, 0);
-        //BlockOffset BlockOffset = new(2, 2);
-        //Console.WriteLine(BlockCoordinate + BlockOffset);
-
-        //BlockCoordinate BlockCoordinate2 = new(0, 0);
-        //Console.WriteLine(BlockCoordinate + Direction.East);
-
-        //Console.WriteLine(BlockCoordinate[0]);
-        //Console.WriteLine(BlockCoordinate[1]);
-
-        //Direction East = Direction.East;
-        //BlockOffset EastOffset = East;
-        //Console.WriteLine(EastOffset);
-
-        //int[] IntArray = [1, 9, 2, 8, 3, 7, 4, 6, 5];
-
-        //List<int> Result1 = ArrayOrganiser.OrderArray1(IntArray);
-        //foreach (int I in Result1)
-        //{
-        //    Console.WriteLine(I);
-        //}
-
-        //IEnumerable<int> Result2 = ArrayOrganiser.OrderArray2(IntArray);
-        //foreach (int I in Result2)
-        //{
-        //    Console.WriteLine(I);
-        //}
-
-        //IEnumerable<int> Result3 = ArrayOrganiser.OrderArray2(IntArray);
-        //foreach (int I in Result3)
-        //{
-        //    Console.WriteLine(I);
-        //}
-
-        //Thread Thread1 = new Thread(CountTo100);
-        //Thread1.Start();
-        //Thread Thread2 = new Thread(CountTo100);
-        //Thread2.Start();
-
-        //Thread1.Join();
-        //Thread2.Join();
-
-        //Console.WriteLine("Main Thread Done");
-
-        //void CountTo100()
-        //{
-        //    for (int i = 0; i < 100; i++)
-        //    {
-        //        Console.WriteLine(i + 1);
-        //    }
-        //}
-
-        RecentNumbers RecentNumbers = new();
-
-        Thread Thread1 = new Thread(RecentNumbers.Generate);
-        Thread1.Start();
-
+        Console.WriteLine("Enter a word for the program to guess");
         while (true)
         {
-            Console.ReadKey(false);
-            if (RecentNumbers.AreNumbersEqual()) Console.WriteLine("You correctly identified a repeated number");
-            else Console.WriteLine("The last two numbers are not the same");
+            string UserInput = Console.ReadLine() ?? "hello";
+            _ = Communicate(UserInput);
         }
-
         
     }
 
-    public class RecentNumbers
+    public static int RandomlyRecreate(string Word)
     {
-        private readonly object _lock = new();
-        public int MostRecentNumber { get; set; }
-        public int SecondMostRecentNumber { get; set; }
-        public void Generate()
-        {
-            Random RandomNumber = new();
-            while (true)
-            {
-                int Num = RandomNumber.Next(10);
-                Console.WriteLine(Num);
-                lock (_lock)
-                {
+        Random Random = new();
 
-                    SecondMostRecentNumber = MostRecentNumber;
-                    MostRecentNumber = Num;
-                }
-                Thread.Sleep(1000);
+        int Counter = 0;
+        string JoinedCharacters = "";
+
+        while (Word != JoinedCharacters)
+        {
+            JoinedCharacters = "";
+            for (int I = Word.Length; I-- > 0;)
+            {
+                JoinedCharacters += ((Char)('a' + Random.Next(26)));  
             }
+            Counter++;
         }
+        return Counter;
+    }
 
-        public bool AreNumbersEqual()
-        {
-            lock (_lock)
-            {
-                return SecondMostRecentNumber == MostRecentNumber;
-            } 
-        }
+    public static Task<int> RandomlyRecreateAsync(string Word)
+    {
+        return Task.Run(() => RandomlyRecreate(Word));
+    }
+
+    public static async Task Communicate(string UserInput)
+    {
+        DateTime BeforeTask = DateTime.Now;
+        Console.WriteLine("Time before task started: " + BeforeTask);
+
+        int NumberOfAttempts = await RandomlyRecreateAsync(UserInput);
+
+        DateTime AfterTask = DateTime.Now;
+        Console.WriteLine("Time when task ended: " + AfterTask);
+        Console.WriteLine("Number of attempts: " + NumberOfAttempts);
+
+        TimeSpan TimeElapsed = AfterTask - BeforeTask;
+        Console.WriteLine("Total time elapsed: " + TimeElapsed);
     }
 }
